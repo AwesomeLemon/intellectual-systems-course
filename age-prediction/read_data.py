b81900ef09716c43f019cc51e8751546e6df99cf
Alexander Chebykin <a.e.chebykin@gmail.com>
2017-12-23 18:36:59 +0300

    Age prediction: fixed data processing => previously negative values became positives because I specified type as uint16. Time wasted. I'm not happy with myself.

--- age-prediction/read_data.py
+++ age-prediction/read_data.py
@@ -47,15 +47,16 @@ def load_friends_birthdays(birth_dates, test_users):
     #     for line in csv.reader(open(os.path.join(graph_path, file)),
     #                            delimiter='\t'):
     #         user = int(line[0])
-    #         # if user in test_users:
-    #         friends_bds = []
-    #         for friendship in line[1][2:len(line[1]) - 2].split('),('):
-    #             parts = friendship.split(',')
-    #             friend = int(parts[0])
-    #             friend_bd = birth_dates[friend]
-    #             friends_bds.append(friend_bd)
-    #         users_to_friends_bds[user] = friends_bds
+    #         if user in test_users:
+    #             friends_bds = []
+    #             for friendship in line[1][2:len(line[1]) - 2].split('),('):
+    #                 parts = friendship.split(',')
+    #                 friend = int(parts[0])
+    #                 friend_bd = birth_dates[friend]
+    #                 friends_bds.append(friend_bd)
+    #             users_to_friends_bds[user] = friends_bds
     # np.save(friends_birth_dates_path, users_to_friends_bds)
+    # return
 
     def save_sparse_csr(filename, array):
         np.savez(filename, data=array.data, indices=array.indices,
@@ -75,7 +76,7 @@ def load_friends_birthdays(birth_dates, test_users):
     links_num = 300292724
     from_user = np.zeros(links_num, dtype=np.int32)
     to_user = np.zeros(links_num, dtype=np.int32)
-    data = np.zeros(links_num, dtype=np.uint16)
+    data = np.zeros(links_num, dtype=np.int16)
     current_pos = 0
     all_users = set()
     i = 0
@@ -88,12 +89,14 @@ def load_friends_birthdays(birth_dates, test_users):
                 for friendship in line[1][2:len(line[1]) - 2].split('),('):
                     parts = friendship.split(',')
                     friend = int(parts[0])
+                    friend_bd = birth_dates[friend]
+                    if friend_bd > 16000 or friend_bd < -16000:
+                        continue
 
                     from_user[current_pos] = user
                     to_user[current_pos] = friend
                     all_users.add(friend)
 
-                    friend_bd = birth_dates[friend]
                     data[current_pos] = friend_bd #((friend_bd + 36500) << 21) + int(parts[1])
 
                     current_pos += 1
@@ -111,24 +114,42 @@ def load_friends_birthdays(birth_dates, test_users):
 def bin(s):
     return str(s) if s <= 1 else bin(s >> 1) + str(s & 1)
 
-def store_features(user_friends_data_matrix, users, birth_dates, path):
+def store_features(user_friends_data_matrix, users, birth_dates, path, is_train=True):
     with open(path, 'w+') as f:
         i = 0
+        sum_bdays = 0
+        cnt_bdays = 0
         for user in users:
-            friends_features = user_friends_data_matrix[user, :].data
-            friends_bds = friends_features#[(friend_and_mask >> 21) - 36500 for friend_and_mask in friends_features]
+            friends_features = user_friends_data_matrix[user, :]
+            friends_bds = friends_features.data#[(friend_and_mask >> 21) - 36500 for friend_and_mask in friends_features]
             cnt_bds = len(friends_bds)
             if cnt_bds == 0:
-                continue
+                if is_train:
+                    continue
+                else:
+                    friends_bds = [4000]
+
             # del friends_features
             friends_bds.sort()
             # [friend_and_mask & 4194303 for friend_and_mask in friends_features]
             sum_bds = sum(friends_bds)
+            # if user == 1811:
+            #     print(user)
+            #     print(friends_features)
+            #     print(friends_bds)
+            #     print(sum_bds)
+            #     print(friends_bds[0])
+            #     print(friends_bds[1])
+            #     print(friends_bds[-1])
+            #     print(friends_bds[-2])
+            #     x = str(raw_input())
             if cnt_bds > 5:
                 sum_middle = sum_bds - friends_bds[0] - friends_bds[1] - friends_bds[-1] - friends_bds[-2]
             else:
                 sum_middle = sum_bds
-            f.write(str(user) + ',' + str(sum_bds) + ',' + str(sum_middle) + ',' + str(cnt_bds) + ',' + str(birth_dates[user]) + '\n')
+            user_bd = birth_dates[user]
+            f.write(str(user) + ',' + str(sum_bds) + ',' + str(sum_middle) + ',' + str(cnt_bds) + ',' + str(
+                user_bd) + '\n')
             i += 1
             if i % 10000 == 0:
                 print(i)
@@ -145,32 +166,39 @@ if __name__ == '__main__':
     test_users = load_test_users()
     birth_dates = load_birth_dates()
     friends_bds, all_users = load_friends_birthdays(birth_dates, test_users)
+
     train_users = all_users - test_users
     # store_features(friends_bds, train_users, birth_dates, extracted_features_train_path)
-    # store_features(friends_bds, test_users, birth_dates, extracted_features_test_path)
-
-    #predicting via average:
+    store_features(friends_bds, test_users, birth_dates, extracted_features_test_path, False)
+    # friends_bds = np.load('data/friends_birthdays.npy')
+    # #predicting via average:
     # with open('pred.txt', 'w') as f:
+    #     # for user in test_users:
     #     for user, user_friends in enumerate(friends_bds):
+    #         # user_friends = friends_bds[user, :].data
     #         if user_friends is None:
     #             continue
-    #         user_friends.sort()
+    #         print(user)
+    #         print(user_friends)
+    #         # user_friends.sort()
     #         if len(user_friends) > 5:
     #             # user_friends = user_friends[int(len(user_friends) * .05): int(len(user_friends) * .95)]
     #             user_friends = user_friends[2: len(user_friends) - 2]
     #         predicted_age = sum(user_friends) / len(user_friends)
     #         # predicted_age = user_friends[len(user_friends) / 2]
     #         f.write(str(user) + ',' + str(predicted_age) + '\n')
-
-    #enriching features via average
-    with open('pred.txt', 'w') as f:
-        for user, user_friends in enumerate(friends_bds):
-            if user_friends is None:
-                continue
-            user_friends.sort()
-            if len(user_friends) > 5:
-                # user_friends = user_friends[int(len(user_friends) * .05): int(len(user_friends) * .95)]
-                user_friends = user_friends[2: len(user_friends) - 2]
-            predicted_age = sum(user_friends) / len(user_friends)
-            # predicted_age = user_friends[len(user_friends) / 2]
-            f.write(str(user) + ',' + str(predicted_age) + '\n')
+    #
+    # #enriching features via average
+    # # with open('pred.txt', 'w') as f:
+    # #     for user, user_friends in enumerate(friends_bds):
+    # #         if user_friends is None:
+    # #             continue
+    # #         user_friends.sort()
+    # #         if len(user_friends) > 5:
+    # #             # user_friends = user_friends[int(len(user_friends) * .05): int(len(user_friends) * .95)]
+    # #             user_friends = user_friends[2: len(user_friends) - 2]
+    # #         user_sum = sum(user_friends)
+    # #         print(user, user_sum)
+    # #         predicted_age = user_sum / len(user_friends)
+    # #         # predicted_age = user_friends[len(user_friends) / 2]
+    # #         f.write(str(user) + ',' + str(predicted_age) + '\n')
